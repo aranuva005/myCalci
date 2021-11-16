01858210208460c0cfbc9040ec7b019efa5b1cef
Itikalamadhavi <itikalamadhavi007@gmail.com>
2021-11-16 13:35:50 +0530

    updating the header file

---
+++ calculator/src/Components/header.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+/**
+ * @author: Madhavi itikala
+ * The below code is to display the no of calculations done by the user.
+ * @param counter:Integer
+ * @returns jsx
+ */
+export function Headers() {
+    const [counter,setCounter] = useState(23);
+     return(
+        <h1>no of calculations:{counter}</h1>
+     )
+
+}
