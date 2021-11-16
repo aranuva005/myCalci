1f6bf4206981deb1bb14fd7637b39ee10a3c33ec
aranuva005 <aranuva@innominds.com>
2021-11-16 18:39:36 +0530

    logic is done

--- calculator/src/Components/body.js
+++ calculator/src/Components/body.js
@@ -9,79 +9,106 @@ import Result from './result'
 
 class Body extends React.Component {
 
-//using props and sending the data
-	constructor(props) {
-		super(props);
+    //using props and sending the data
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			result: "",
-            keypad:[{
-                name :"%" 
-            },{
-                name :"/" 
-            },{
-                name :"7" 
-            },{
-                name :"8" 
-            },{
-                name :"9" 
-            },{
-                name :"*" 
-            },{
-                name :"4" 
-            },{
-                name :"5" 
-            },{
-                name :"6" 
-            },{
-                name :"-" 
-            },{
-                name :"1" 
-            },{
-                name :"2" 
-            },{
-                name :"3" 
-            },{
-                name :"+" 
-            },{
-                name :"0" 
-            },{
-                name :"." 
+        this.state = {
+            result: "",
+            keypad: [{
+                name: "%"
+            }, {
+                name: "/"
+            }, {
+                name: "7"
+            }, {
+                name: "8"
+            }, {
+                name: "9"
+            }, {
+                name: "*"
+            }, {
+                name: "4"
+            }, {
+                name: "5"
+            }, {
+                name: "6"
+            }, {
+                name: "-"
+            }, {
+                name: "1"
+            }, {
+                name: "2"
+            }, {
+                name: "3"
+            }, {
+                name: "+"
+            }, {
+                name: "0"
+            }, {
+                name: "."
             }]
-		}
-	}
-// This is an event handler, when an button is clicked the function in event handler will be executed.
-	onClick = button => {
-		if(button === "="){
-			this.calculate()
-	}
+        }
+    }
+    // This is an event handler, when an button is clicked the function in event handler will be executed.
+    onClick = button => {
+        if (button === "=") {
+            this.calculate()
+        }
 
-	else if(button === "AC"){
-			this.reset()
-	}
-	else if(button === "BC"){
-			this.backspace()
-	}
+        else if (button === "AC") {
+            this.reset()
+        }
+        else if (button === "BC") {
+            this.backspace()
+        }
 
-	else {
-			this.setState({
-					result: this.state.result + button
-			})
-	}
-	};
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+    // This is an event handler, when an button is clicked the function in event handler will be executed.
+    // eval function evaluates or executes an argument
+    calculate = () => {
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(this.state.result) || "") + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
 
-	render() {
-		// const { displayValue } = this.state;
-		return (
-			<>
-				<div class="container">
-					< Result result={this.state.result} />
-					< KeyPadComponent onClick={this.onClick} matter = "button num-button"  keypad = {this.state.keypad}/>
+        }
+    };
 
-				</div>
-			</>
-		)
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
 
-	}
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        // const { displayValue } = this.state;
+        return (
+            <>
+                <div class="container">
+                    < Result result={this.state.result} />
+                    < KeyPadComponent onClick={this.onClick} matter="button num-button" keypad={this.state.keypad} />
+
+                </div>
+            </>
+        )
+
+    }
 }
 export default Body;
